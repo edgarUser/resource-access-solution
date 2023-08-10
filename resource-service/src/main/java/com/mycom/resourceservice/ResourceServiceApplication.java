f18a51ce1e7b8da6b2f4e764822f59ea2cc8ce81
edgarUser <edgar.lopez.isc@gmail.com>
2023-08-10 17:12:33 -0600

    Fixing package configuration

--- resource-service/src/main/java/com/mycom/resourceservice/ResourceServiceApplication.java
+++ resource-service/src/main/java/com/mycom/resourceservice/ResourceServiceApplication.java
@@ -1,9 +1,12 @@
+package com.mycom.resourceservice;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ResourceServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ResourceServiceApplication.class, args);
-    }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ResourceServiceApplication.class, args);
+  }
 }
