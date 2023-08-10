ffd97040f0e08f7a8bcd35d395ceb41e2fc225c0
edgarUser <edgar.lopez.isc@gmail.com>
2023-08-10 17:26:51 -0600

    Adding trust-client module

---
+++ trust-client/src/main/java/com/mycom/trustclient/TrustClientApplication.java
@@ -0,0 +1,12 @@
+package com.mycom.trustclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrustClientApplication.class, args);
+  }
+}
