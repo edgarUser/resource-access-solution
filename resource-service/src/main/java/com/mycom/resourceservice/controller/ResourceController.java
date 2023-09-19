bc12aa62fbb92df0f57c961370efc12d6208f8e0
edgarUser <edgar.lopez.isc@gmail.com>
2023-09-19 15:42:17 -0600

    Adding basic auth

--- resource-service/src/main/java/com/mycom/resourceservice/controller/ResourceController.java
+++ resource-service/src/main/java/com/mycom/resourceservice/controller/ResourceController.java
@@ -4,6 +4,7 @@ import com.mycom.resourceservice.model.Resource;
 import com.mycom.resourceservice.restclient.TrustServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
