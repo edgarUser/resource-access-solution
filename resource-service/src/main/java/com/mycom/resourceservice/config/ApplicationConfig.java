a0d6f3f7da1aec80bfef2c538aec00c578ae981d
edgarUser <edgar.lopez.isc@gmail.com>
2023-09-19 16:36:12 -0600

    Enabling token introspect

--- resource-service/src/main/java/com/mycom/resourceservice/config/ApplicationConfig.java
+++ resource-service/src/main/java/com/mycom/resourceservice/config/ApplicationConfig.java
@@ -1,17 +1,11 @@
 package com.mycom.resourceservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,7 +13,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class ApplicationConfig {
 
+    @Value("${application.auth.introspect.uri}")
+    private String authUri;
+    @Value("${application.auth.introspect.clientId}")
+    private String clientId;
+    @Value("${application.auth.introspect.clientSecret}")
+    private String clientSecret;
+
     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .opaqueToken(opaqueToken -> opaqueToken
+                                .introspectionUri(authUri)
+                                .introspectionClientCredentials(clientId, clientSecret)));
+        return http.build();
+    }
+
+
+    /*@Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
 
         UserDetails admin = User.withUsername("admin")
@@ -47,6 +60,6 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
+    }*/
 
 }
